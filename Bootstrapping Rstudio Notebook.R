0ec46c3e39804142c2bed43ea3167d0381e4afe9
TStoian <tudorpaul.stoian@gmail.com>
2018-02-12 18:32:45 -0500

    In preparation of the first long presentation
    I am putting up what I have managed to test and get working. This should allow me to illustrate where I stand and where I am still struggling.

--- Bootstrapping Rstudio Notebook.R
+++ Bootstrapping Rstudio Notebook.R
@@ -1,5 +1,6 @@
 
 #For testing purposes I will use the dataset used in the book "An Introduction to the Bootstrap " by Bradley Effron and Robert J. Tibshirani
+install.packages("bootstrap") #if the R studio in question does not already have it installed.
 library(bootstrap)
 data("law")
 
@@ -22,3 +23,59 @@ for(i in 1:k){
 }
 
 print(counter)
+#Seems like this works
+
+
+# Here I'll try to compute the relevant statistic for a resampling, obviously how difficult this is depends on the statistic in question
+#For now, I'll try to implement the mean and median for the resamples.
+sample1=c()
+for(i in 1:15){
+  sample1[i]=sample( 1:15, 1)
+}
+mean(sample1)
+median(sample1)
+
+#Here I'll extend the algorithm to perform to generate B samples and generate the 
+B= 1000 #This is the number of bootstrap replications 
+
+matrix_of_statistics=c()
+for(j in 1:B ){
+  sample1=c() #We have an option here, we could commit to memory all the resamplings we obtained, but really we only care about the sample statistic
+  for(i in 1:15){
+    sample1[i]= law$GPA[ sample( 1:15, 1)] 
+  }
+  matrix_of_statistics[j]= mean(sample1)
+}
+hist(matrix_of_statistics)
+summary(matrix_of_statistics)
+#Question to ponder: is it worth sorting the data ahead of processing? At first it seems harmless, but since we resample, perhaps it has no effect
+
+
+#I can create an empirical 95% confidence interval by ordering these statistics, and then choosing the relevant percentile
+sorted_statistics= sort(matrix_of_statistics)
+
+left_end= sorted_statistics[26]
+right_end= sorted_statistics[976]
+
+interval_statistics=c(left_end,right_end)
+interval_statistics
+
+hist(law$GPA)
+t.test(law$GPA )
+median(law$GPA)
+
+#Next I compute the standard error using the bootstrap, as detailed in Effron and Tibshirani
+
+boot_mean=bootstrap(law$GPA,1000,mean)
+hist(boot_mean$thetastar)
+
+
+
+
+#There are a number of complications with using the built-in 
+
+#abcnon(law$GPA,mean, alpha = 0.05)
+
+#Might be worth getting more data, and I know Stat2 has a lot of it available.
+library(Stat2Data)
+data("Cereal")
